1d055dee1323d6807496fe6d790cd79763c1c1a3
maripezarini <53429372+maripezarini@users.noreply.github.com>
2019-12-10 21:07:08 -0300

    Aula 02 - 10/12

--- Aula01/app.js
+++ Aula01/app.js
@@ -2,7 +2,7 @@ const module1 = require('./mod1.js')
 //const module2 = require('./mod2.js')
 
 const {message} = require('./mod2.js')
-
+//quando vc usa as chaves na hora de chamar o módulo, chama 'destructor'
 //dá pra chamar o objeto direto, em vez de chamar primeiro o arquivo e dpeois o objeto
 
 //diferenças entre var, let e const -- var e let podem ser reatribuidos e vão ser sobrescrevidas sem que dê nenhum erro no código. Quando você usa a const (constante), pois o erro é dito logo na compilação - o que super melhora o código.
--- Aula01/appServer.js
+++ Aula01/appServer.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const requisicao = function(request, response) {
+const requisicao = function(_, response) {
     response.writeHead(200, {"Content-Type":"text/html"})
     response.write('<h1>Texto a ser exibido no browser</h1>')
     response.end()
